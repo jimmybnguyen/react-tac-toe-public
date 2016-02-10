1ad07d50b03c8d01941a637f124ce0ea5f7bdfd4
jimmybnguyen <me@jimmybnguyen.com>
2016-02-10 00:23:27 -0800

    add comments to game logic

--- app/game.js
+++ app/game.js
@@ -31,14 +31,16 @@ class TicTacToe {
     }
     
     gameWon() {
+        //checks the top row
         if (this._gameBoard[0] == '1' &&
             this._gameBoard[1] == '1' &&
             this._gameBoard[2] == '1') {
-            console.log('won 1st row 1');   
+            console.log('won 1st row 1');
         } else if (this._gameBoard[0] == '2' &&
                    this._gameBoard[1] == '2' &&
                    this._gameBoard[2] == '2') {
-            console.log('won 1st row 2');   
+            console.log('won 1st row 2');  
+        //checks the middle row
         } else if (this._gameBoard[3] == '1' &&
                    this._gameBoard[4] == '1' &&
                    this._gameBoard[5] == '1') {
@@ -47,6 +49,7 @@ class TicTacToe {
                    this._gameBoard[4] == '2' &&
                    this._gameBoard[5] == '2') {
             console.log('won 2nd row 2');   
+        //checks the bottom row
         } else if (this._gameBoard[6] == '1' &&
                    this._gameBoard[7] == '1' &&
                    this._gameBoard[8] == '1') {
@@ -54,7 +57,8 @@ class TicTacToe {
         } else if (this._gameBoard[6] == '2' &&
                    this._gameBoard[7] == '2' &&
                    this._gameBoard[8] == '2') {
-            console.log('won 3rd row 2');   
+            console.log('won 3rd row 2'); 
+        //checks the left column
         } else if (this._gameBoard[0] == '1' &&
                    this._gameBoard[3] == '1' &&
                    this._gameBoard[6] == '1') {
@@ -62,15 +66,17 @@ class TicTacToe {
         } else if (this._gameBoard[0] == '2' &&
                    this._gameBoard[3] == '2' &&
                    this._gameBoard[6] == '2') {
-            console.log('won 2nd column 2');   
+            console.log('won 1st column 2');  
+        //checks the middle column
         } else if (this._gameBoard[1] == '1' &&
                    this._gameBoard[4] == '1' &&
                    this._gameBoard[7] == '1') {
-            console.log('won 2nd column 2');   
+            console.log('won 2nd column 1');   
         } else if (this._gameBoard[1] == '2' &&
                    this._gameBoard[4] == '2' &&
                    this._gameBoard[7] == '2') {
             console.log('won 2nd column 2');   
+        //checks the right column
         } else if (this._gameBoard[2] == '1' &&
                    this._gameBoard[5] == '1' &&
                    this._gameBoard[8] == '1') {
@@ -78,7 +84,8 @@ class TicTacToe {
         } else if (this._gameBoard[2] == '2' &&
                    this._gameBoard[5] == '2' &&
                    this._gameBoard[8] == '2') {
-            console.log('won 3rd column 2');   
+            console.log('won 3rd column 2');  
+        //checks the left to right diagonal
         } else if (this._gameBoard[0] == '1' &&
                    this._gameBoard[4] == '1' &&
                    this._gameBoard[8] == '1') {
@@ -86,15 +93,18 @@ class TicTacToe {
         } else if (this._gameBoard[0] == '2' &&
                    this._gameBoard[4] == '2' &&
                    this._gameBoard[8] == '2') {
-            console.log('won left diagonal 2');   
+            console.log('won left diagonal 2');  
+        //checks the right to left diagonal
         } else if (this._gameBoard[2] == '1' &&
                    this._gameBoard[4] == '1' &&
                    this._gameBoard[6] == '1') {
-            console.log('won right diagonal 1');   
+            console.log('won right diagonal 1'); 
         } else if (this._gameBoard[2] == '2' &&
                    this._gameBoard[4] == '2' &&
                    this._gameBoard[6] == '2') {
             console.log('won right diagonal 2');   
+        } else if (this._gameBoard.length = 9) {
+            console.log('tied');   
         }
     }
 }
